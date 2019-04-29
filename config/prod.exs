327ff8e9764309cf81f79c57700fe636a210b287
AlexanderDmitriev <alexanderdmv@gmail.com>
2019-04-29 16:18:15 +0300

    fix config for deploy

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,8 @@ config :youecho, YouechoWeb.Endpoint,
   http: [port: 4030],
   url: [host: "localhost", port: 4030],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  server: true,
+  code_reloader: false
 
 # Do not print debug messages in production
 config :logger, level: :info
